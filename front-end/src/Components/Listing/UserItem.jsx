c302b0ea57ef7fe97673649a983964c1b88b594c
JohnYu2000 <junye.yu.2@gmail.com>
2024-04-02 17:01:40 -0600

    Added a toggle button

---
+++ front-end/src/Components/Listing/UserItem.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Listing.css'
+
+function UserItem({id, name, email}) {
+
+    const navigate = useNavigate();
+
+    return (
+        <div id="listing-container" style={{cursor: 'pointer'}} onClick={() => {
+            navigate(`/viewprofile/:${email}/`)
+        }}>
+            <div className="listing-item-title">
+                {name}
+            </div>
+            <div className="list-item-price">
+                {email}
+            </div>
+        </div>
+    )
+}
+
+export default UserItem;
