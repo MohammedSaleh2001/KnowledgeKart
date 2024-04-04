10feb93b7430a6efec0fe3965ebfd2da1de13f94
JohnYu2000 <junye.yu.2@gmail.com>
2024-04-04 12:45:30 -0600

    Created the investigate form

--- front-end/src/Components/Moderate/ModerateInvestigate.jsx
+++ front-end/src/Components/Moderate/ModerateInvestigate.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import './Moderate.css'
 
 function ModerateInvestigate() {
 
+    const navigate = useNavigate();
+
     const { userID } = useParams();
 
     return (
-        <>
-        </>
+        <div id="moderate_investigate_container">
+            <div id="title">
+                Investigation Form
+            </div>
+            <div id="email_container">
+                <input type="email" placeholder="Enter Email" readonly="readonly"/>
+            </div>
+            <div id="textarea_container">
+                <textarea placeholder="Type Your Rationale..." readonly="readonly"/>
+            </div>
+            <div id="action_buttons_div">
+                <div onClick={() => {
+                    navigate('/moderateview');
+                }}>
+                    Back
+                </div>
+                <div>
+                    View Profile
+                </div>
+                <div>
+                    View Chat History
+                </div>
+                <div id="suspend_button">
+                    Suspend
+                </div>
+            </div>
+        </div>
     )
 }
 
